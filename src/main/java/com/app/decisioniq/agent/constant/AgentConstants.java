b1a633cc344d650e0556deeb3bd11037d228676a
prashanth yp <prashanth.yp89@gmail.com>
2026-06-14 16:16:29 +0530

    dq-agent-pilot: Changed package structure

--- src/main/java/com/app/decisioniq/agent/constant/AgentConstants.java
+++
@@ -1,8 +0,0 @@
-package com.app.decisioniq.agent.constant;
-
-public class AgentConstants {
-
-    public static final String INTENT="intent";
-    public static final String QUERY="query";
-    public static final String ANSWER="answer";
-}
